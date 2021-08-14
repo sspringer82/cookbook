dc840bfac744d7b4cb187c7e9ef21b6b1b85b817
Sebastian Springer <88656+sspringer82@users.noreply.github.com>
2021-08-14 20:46:08 +0200

    Komponenten

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Komponente1 from './Komponente1';
+import Komponente2 from './Komponente2';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello React</h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Komponente1 />
+      <Komponente2 />
     </div>
   );
 }
---
+++ src/Komponente1.tsx
@@ -0,0 +1,3 @@
+export default function Komponente1() {
+  return <h1>Hallo React!</h1>;
+}
