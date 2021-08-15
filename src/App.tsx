3d682361d694f7607e91a11430595eddb7c3b3b6
Sebastian Springer <88656+sspringer82@users.noreply.github.com>
2021-08-15 09:41:12 +0200

    recipe list

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,8 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import RecipeList from './RecipeList';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello React</h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <RecipeList />;
 }
 
 export default App;
---
+++ src/RecipeList.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function RecipeList(): React.ReactElement {
+  return <h1>Rezeptliste</h1>;
+}
+
+export default RecipeList;
