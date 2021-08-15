234986bcd004f508369761c22e86d15725ea954f
Sebastian Springer <88656+sspringer82@users.noreply.github.com>
2021-08-15 10:11:19 +0200

    recipes

---
+++ src/recipes.ts
@@ -0,0 +1,42 @@
+import Recipe from './types/recipe';
+
+const recipes: Recipe[] = [
+  {
+    id: 1,
+    title: 'Schnitzel',
+    ingredients: [
+      {
+        title: 'Fleisch',
+        amount: 1,
+        unit: 'kg',
+      },
+      {
+        title: 'Panade',
+        amount: 0.5,
+        unit: 'kg',
+      },
+    ],
+    steps: ['panieren', 'braten'],
+    rating: 5,
+  },
+  {
+    id: 2,
+    title: 'Bratkartoffeln',
+    ingredients: [
+      {
+        title: 'Kartoffeln',
+        amount: 2,
+        unit: 'kg',
+      },
+      {
+        title: 'Zwiebeln',
+        amount: 0.5,
+        unit: 'kg',
+      },
+    ],
+    steps: ['Kartoffeln schälen', 'braten'],
+    rating: 5,
+  },
+];
+
+export default recipes;
