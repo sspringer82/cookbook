5f93b85482eb90324c2d34a167c6e24932a783f0
Sebastian Springer <88656+sspringer82@users.noreply.github.com>
2021-08-18 21:02:14 +0200

    mount  hook

--- src/RecipeList.tsx
+++ src/RecipeList.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import RecipeListItem from './RecipeListItem';
 import initRecipes from './recipes';
 import { Recipe } from './types/Recipe';
@@ -6,7 +6,13 @@ import { Recipe } from './types/Recipe';
 function RecipeList(): React.ReactElement {
   const [headline] = useState<string>('Rezeptliste');
 
-  const [recipes, setRecipes] = useState<Recipe[]>(initRecipes);
+  const [recipes, setRecipes] = useState<Recipe[]>([]);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setRecipes(initRecipes);
+    }, 1000);
+  }, []);
 
   function handleDelete(id: number) {
     setRecipes((prevRecipes) => {
