cc4f9c73927571a94f407c8f264cf447f63fe3df
Sebastian Springer <88656+sspringer82@users.noreply.github.com>
2021-08-28 05:30:37 +0200

    class components

---
+++ src/RecipeList.class.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class RecipeList extends React.Component {
+  render() {
+    const headline = 'Mein Kochbuch!';
+
+    return <h1>{headline}</h1>;
+  }
+}
+
+export default RecipeList;
